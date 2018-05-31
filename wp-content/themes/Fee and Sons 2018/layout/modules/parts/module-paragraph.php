eee2c2d3b19f3056da7a4b8c8ee7c693e6e629c7
Russ Fee <russfee@gmail.com>
2018-05-31 11:18:52 -0600

    plugins and images

--- wp-content/themes/Fee and Sons 2018/layout/modules/parts/module-paragraph.php
+++ wp-content/themes/Fee and Sons 2018/layout/modules/parts/module-paragraph.php
@@ -2,7 +2,9 @@
 
 <div class="row constrain" style="<?php the_sub_field('row_padding_top'); ?><?php the_sub_field('row_padding_bottom'); ?>">
 	<div style="margin: 0 auto; width: <?php the_sub_field('text_width'); ?>; text-align: <?php the_sub_field('text_align'); ?>; color: <?php the_sub_field('text_colour'); ?>; font-size: <?php the_sub_field('text_size'); ?>;">
-	<?php the_sub_field('paragraph'); ?>
+	<div class="paragraph-container">
+		<?php the_sub_field('paragraph'); ?>
+	</div> <!--paragraph-container-->
 	</div>
 	
 </div> <!--row-->
--- wp-content/themes/Fee and Sons 2018/layout/modules/parts/module-sectioned_content.php
+++ wp-content/themes/Fee and Sons 2018/layout/modules/parts/module-sectioned_content.php
@@ -1,15 +1,15 @@
 <?php // MODULE - SECTIONED CONTENT ?>
 			
-			<div class="row constrain" style="<?php the_sub_field('row_padding_top'); ?><?php the_sub_field('row_padding_bottom'); ?>">
+			<div class="row" style="<?php the_sub_field('row_padding_top'); ?><?php the_sub_field('row_padding_bottom'); ?>">
 
 <?php if(get_sub_field('sectioned_content_repeater')): ?>
 				
 				
 					<?php while(has_sub_field('sectioned_content_repeater')): ?>
 	<div class="sectioned-content-container">
-		<h3 style="padding: 0 0 20px 0;"><?php the_sub_field('sectioned_title'); ?></h3>
 		<div class="sectioned-image" style="background-image: url(<?php the_sub_field('sectioned_image'); ?>)"></div> <!--sectioned-image-->
-		<?php the_sub_field('sectioned_paragraph'); ?>
+		<h3 style="padding: 20px 0 0 40px;"><?php the_sub_field('sectioned_title'); ?></h3>
+		<div style="margin: 40px;"><?php the_sub_field('sectioned_paragraph'); ?></div>
 	</div> <!--news-story-container-->
 					<?php endwhile; ?>
 				<?php endif; ?>
